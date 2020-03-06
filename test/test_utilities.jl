55551677bed53a37b07a7e93ec69cc11963cea24
Benedikt Ehinger <behinger@uos.de>
2020-03-06 13:51:52 +0100

    find out pwd

--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -1,5 +1,6 @@
 using CSV
 function loadtestdata(testCase::String)
+    println(pwd()) # to debug github action
     data = CSV.read("./test/data/"*testCase*"_data.csv", header=0)
     data = convert(Matrix,data)
     data = dropdims(data,dims=1) # convert to vector
