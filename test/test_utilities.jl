6f4762f01f9def28b53a164f23fa2ff32049b3bb
Benedikt Ehinger <behinger@uos.de>
2020-03-06 15:13:39 +0100

    changed default folder for testing to work with github

--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -1,9 +1,9 @@
 using CSV
 function loadtestdata(testCase::String)
-    println(pwd()) # to debug github action
-    data = CSV.read("./test/data/"*testCase*"_data.csv", header=0)
+    #println(pwd()) # to debug github action
+    data = CSV.read("./data/"*testCase*"_data.csv", header=0)
     data = convert(Matrix,data)
     data = dropdims(data,dims=1) # convert to vector
-    evts = CSV.read("./test/data/"*testCase*"_events.csv");
+    evts = CSV.read("./data/"*testCase*"_events.csv");
     return data,evts
 end
