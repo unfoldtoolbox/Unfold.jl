dfe9ff7d7f71109649711e8e9b60f8fc3479fdbd
Benedikt Ehinger <behinger@uos.de>
2020-03-08 14:20:08 +0100

    allow to be used in docs

--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -1,9 +1,9 @@
 using CSV
-function loadtestdata(testCase::String)
+function loadtestdata(testCase::String,dataPath::String="")
     #println(pwd()) # to debug github action
-    data = CSV.read("./data/"*testCase*"_data.csv", header=0)
+    data = CSV.read(dataPath*"data/"*testCase*"_data.csv", header=0)
     data = convert(Matrix,data)
     data = dropdims(data,dims=1) # convert to vector
-    evts = CSV.read("./data/"*testCase*"_events.csv");
+    evts = CSV.read(dataPath*"data/"*testCase*"_events.csv");
     return data,evts
 end
