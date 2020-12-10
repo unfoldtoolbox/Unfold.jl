de260e1e9a4125f29187ae3a2fb32aa0ed928e31
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2020-12-10 19:56:35 +0100

    bugfix change in CSV.read

--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -1,9 +1,10 @@
 using CSV
+using DataFrames
 function loadtestdata(testCase::String,dataPath::String="")
     #println(pwd()) # to debug github action
-    data = CSV.read(dataPath*"data/"*testCase*"_data.csv", header=0)
+    data = CSV.read(dataPath*"data/"*testCase*"_data.csv", DataFrame,header=0)
     data = convert(Matrix,data)
     data = dropdims(data,dims=1) # convert to vector
-    evts = CSV.read(dataPath*"data/"*testCase*"_events.csv");
+    evts = CSV.read(dataPath*"data/"*testCase*"_events.csv",DataFrame);
     return data,evts
 end
