16fab6697b7dd3d40e9ff275cfd85090c922c2aa
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-02-15 13:57:26 +0100

    fixed timing bugs, added optional times

--- src/predict.jl
+++ src/predict.jl
@@ -1,6 +1,8 @@
 import StatsBase.predict
 
-function predict(model::UnfoldLinearModel, newdata)
+
+
+function predict(model::UnfoldLinearModel, newdata,times=nothing)
     # make a copy of it so we don't change it outside the function
      data = copy(newdata)
     #data = newdata
@@ -13,59 +15,83 @@ function predict(model::UnfoldLinearModel, newdata)
     # add those as latencies [xxx change to basisfunction field] to newdata
     ##
 
-    X = []
-    fromTo = []
-    times = []
     if !(typeof(formulas[1].rhs) <: unfold.TimeExpandedTerm)
         # mass univariate model. Not sure how I can multiple dispatch correctly :| Maybe I need 4 types after all
+
+        # just a single designmat, same for all timepoints
         X = designmatrix(UnfoldLinearModel,formulas[1],data)
+
+        # setup the output matrix, has to be a matrix
         yhat = Array{Union{Missing,Float64}}(missing,size(model.beta,1),size(X.Xs,1),size(model.beta,2))
-        #yhat = zeros(size(model.beta,1),size(X.Xs,1),size(model.beta,2))
+
+        
         for ch = 1:size(model.beta,1)
             yhat[ch,:,:] = X.Xs * permutedims(model.beta[ch,:,:],(2,1))
         end
+
         # fake times because we never save it in the object
-        times = repeat(range(1,length=size(yhat,3)),size(data,1))
+        timesVec= repeat(range(1,length=size(yhat,3)),size(data,1))
+        if !isnothing(times)
+            timesVec = times[timesVec]
+        end
+        # but times could be optionally given
+
         # reshape yhat
         
         yhat = reshape(permutedims(yhat,(1,3,2)),size(yhat,1),:)
         yhat = permutedims(yhat,(2,1))
     else
 
-    for f = formulas
+        X = []
+        fromTo = []
+        timesVec = []
+        for f = formulas
 
-        # find out how long each designmatrix is
-        n_range = length(f.rhs.basisfunction.times)
-        # find out how much to shift so that X[1,:] is the the first "sample"
-        n_negative = f.rhs.basisfunction.shiftOnset
-        # generate the correct eventfields (default: latencies)
-        data[:,f.rhs.eventfields[1]] = range(-n_negative + 1, step=n_range, length=size(data, 1))
-        
+            # find out how long each designmatrix is
+            n_range = length(f.rhs.basisfunction.times)
+            # find out how much to shift so that X[1,:] is the the first "sample"
+            n_negative = f.rhs.basisfunction.shiftOnset
+            # generate the correct eventfields (default: latencies)
+            data[:,f.rhs.eventfields[1]] = range(-n_negative + 1, step=n_range, length=size(data, 1))
+            
 
-        
-        # get the model
-        Xsingle = modelcols(f.rhs, data)
-        # combine designmats
-        append!(X, [Xsingle])
+            
+            # get the model
+            Xsingle = modelcols(f.rhs, data)
 
-        # keep track of how long each event is
-        append!(fromTo, [range(1, step=n_range, length=1+size(data, 1))])
+            # remove the last time-point because it is attached due to non-integer latency/eventonsets.
+            # e.g. x denotes a sample
+            # x- - -x- - -x- - -x
+            # e- - - -f- - - -g-
+            # 
+            # e is aligned (integer) with the sample
+            # f&g are between two samples, thus the design matrix would interpolate between them. Thus has as a result, that the designmatrix is +1 longer than what would naively be expected
+            #
+            # because in "predict" we define where samples onset, we can remove the last sample, it s always 0 anyway, but to be sure we test it
+            @assert Xsingle[end,end] == 0.0
+            Xsingle = Xsingle[1:end-1,:] 
+            
+            # combine designmats
+            append!(X, [Xsingle])
 
-        # keep track of the times
-        append!(times, repeat(f.rhs.basisfunction.times[1:end], size(data, 1)))
+            # keep track of how long each event is
+            append!(fromTo, [range(1, step=n_range, length=size(data, 1))])
 
-    end
+            # keep track of the times
+            append!(timesVec, repeat(f.rhs.basisfunction.times[1:end], size(data, 1)))
+
+        end
     
-    # Concat them, but without introducing any overlap, we want the isolated responses
-    Xconcat = blockdiag(X...)
-    
-    # calculate yhat
-    # yhat x channels
-    yhat =  Xconcat * model.beta'
+        # Concat them, but without introducing any overlap, we want the isolated responses
+        Xconcat = blockdiag(X...)
+        
+        # calculate yhat
+        # yhat x channels
+        yhat =  Xconcat * model.beta'
     end
     
     # init the meta dataframe
-    metaData = DataFrame([:times => vcat(times...),:basisname=>""])
+    metaData = DataFrame([:times => vcat(timesVec...),:basisname=>""])
     for c = names(newdata)
         metaData[:,c] .= newdata[1,c] # assign first element in order to have same column type
     end
