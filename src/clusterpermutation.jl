27b297429fbcc3db8682c34304bce331010b9110
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-03-23 14:47:08 +0100

    stop early if no clusters in sight (no need to permute)

--- src/clusterpermutation.jl
+++ src/clusterpermutation.jl
@@ -22,14 +22,21 @@ function cluster_permutation_test(mres::UnfoldLinearMixedModel,
 tRange = findfirst(times.>=test_times[1]):findfirst(times.>=test_times[2])
 
 
-# this is the meat of the function.
-permDat = cluster_permutation(mres,dat,tRange,coeffOfInterest,nPerm)
-
 # extract observed z-values
 z_obs = [m.z for m in coefpvalues(mres.modelinfo) if String(m.coefname)==coefnames(mres.X.formulas)[2][coeffOfInterest]]
 
 # cluster observed
-obs_cluster = pymne_cluster(z_obs,clusterFormingThreshold;adjacency=adjacency)
+obs_cluster = pymne_cluster(z_obs,clusterFormingThreshold;tRange = tRange,adjacency=adjacency)
+
+# if there is no cluster in the real data, return empty p_df
+if isempty(obs_cluster[2])
+    p_df = DataFrame(:from=>[],:to=>[],:pval=>[],:coefname=>[])
+    return p_df
+end
+
+# this is the meat of the function.
+permDat = cluster_permutation(mres,dat,tRange,coeffOfInterest,nPerm)
+
 
 # cluster permuted
 perm_cluster = [pymne_cluster(permDat[:,p],clusterFormingThreshold;adjacency=adjacency) for p = 1:nPerm]
@@ -43,7 +50,15 @@ p_vals = PyMNE.stats.cluster_level._pval_from_histogram(obs_cluster[2], perm_H0,
 
 fs = 1 ./Float64(times.step)
 # get p-values, we have to shift by the test_times[1] though
-p_df = DataFrame(:from=>[o[1].start ./ fs - test_times[1] for o in obs_cluster[1]],:to=>[o[1].stop ./fs - test_times[1] for o in obs_cluster[1]],:pval=>p_vals)
+if clusterFormingThreshold == "tfce"
+    # not sure how to handle this nicer...
+    fromList = [o.start ./ fs - test_times[1] for o in obs_cluster[1]]
+    toList = [o.stop ./fs - test_times[1] for o in obs_cluster[1]]
+else
+    fromList = [o[1].start ./ fs - test_times[1] for o in obs_cluster[1]]
+toList = [o[1].stop ./fs - test_times[1] for o in obs_cluster[1]]
+end
+p_df = DataFrame(:from=>fromList,:to=>toList,:pval=>p_vals)
 
 p_df[!,:coefname] .= coefnames(mres.X.formulas)[2][coeffOfInterest] # yeah this might break :S
 return p_df
@@ -58,12 +73,15 @@ end
 
 function cluster_permutation(mres,dat,tRange,coeffOfInterest,nPerm)
     permDat = Matrix{Float64}(undef,length(tRange),nPerm)
-    mm = unfold.LinearMixedModel_wrapper(mres.X.formulas,dat[1,1,:],mres.X.Xs)
+    mm_outer = unfold.LinearMixedModel_wrapper(mres.X.formulas,dat[1,1,:],mres.X.Xs)
     
     chIx = 1 # for now we only support 1 channel anyway
+    #
+    #Threads.@threads
     @showprogress "Processing Timepoints" for tIx =1:length(tRange)
         println(tIx)
         # splice in the correct data for residual calculation
+        mm = deepcopy(mm_outer)
         mm.y .= dat[chIx,tRange[tIx],:]
         
         # set the previous calculated model-fit
@@ -75,7 +93,7 @@ function cluster_permutation(mres,dat,tRange,coeffOfInterest,nPerm)
         H0[coeffOfInterest] = 0
         # run the permutation
         # important here is to set the same seed to keep flip all time-points the same
-        perm = permutation(MersenneTwister(1),nPerm,mm;β=H0,blup_method=olsranef); # constant rng to keep autocorr & olsranef for singular models
+        perm = permutation(MersenneTwister(1),nPerm,mm;β=H0,blup_method=olsranef,use_threads=false); # constant rng to keep autocorr & olsranef for singular models
         
         # extract the z-value
         perm_z = [m.z for m in perm.coefpvalues if String(m.coefname)==coefnames(mm)[coeffOfInterest]]
@@ -88,7 +106,9 @@ function cluster_permutation(mres,dat,tRange,coeffOfInterest,nPerm)
 
 
     # function to call pymne -> _find_cluster function
-    function pymne_cluster(data,clusterFormingThreshold;adjacency=nothing)
-        return PyMNE.stats.cluster_level._find_clusters(data,clusterFormingThreshold,adjacency=adjacency)
+    function pymne_cluster(data,clusterFormingThreshold;tRange = 1:size(data,1), adjacency=nothing)
+
+        return PyMNE.stats.cluster_level._find_clusters(data,clusterFormingThreshold,adjacency=adjacency,include=tRange)
     end
     
+    pymne_cluster(data,clusterFormingThreshold::String;kwargs...) = pymne_cluster(data,Dict(:start=>0,:step=>0.2);kwargs...)
