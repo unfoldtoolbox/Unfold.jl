09d8c0578e2216fffb74879b83a2b64849f652e8
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-04-28 17:54:24 +0200

    moved docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,12 @@ using Documenter
 using unfold
 using DocStringExtensions
 makedocs(sitename="Unfold.jl",
-        root = joinpath(dirname(pathof(unfold)), "..", "doc"),
+        root = joinpath(dirname(pathof(unfold)), "..", "docs"),
         prettyurls = get(ENV, "CI", nothing) == "true",
         pages = [
             "index.md",
             "LM Tutorial" =>"lm_tutorial.md",
            # "LMM Tutorial" =>"lmm_tutorial.md",
         ])
+
+deploydocs(; repo = "github.com/unfoldtoolbox/unfold.jl", push_preview = true)
