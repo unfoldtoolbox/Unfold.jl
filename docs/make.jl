e81f16997b98202d748d38e8ce51e60e896e5dec
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-05-02 20:15:05 +0200

    and once more

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,10 @@ gr()
 makedocs(sitename="Unfold.jl",
         root = joinpath(dirname(pathof(Unfold)), "..", "docs"),
         prettyurls = get(ENV, "CI", nothing) == "true",
-        devbranch = "main",
         pages = [
             "index.md",
             "LM Tutorial" =>"lm_tutorial.md",
             "LMM Tutorial" =>"lmm_tutorial.md",
         ])
 
-deploydocs(; repo = "github.com/unfoldtoolbox/Unfold.jl", push_preview = true)
+deploydocs(; repo = "github.com/unfoldtoolbox/Unfold.jl", push_preview = true,        devbranch = "main")
