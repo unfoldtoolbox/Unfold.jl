ee9ee3d32eabd25e12c6f0d487d8a6de15deca05
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-09-29 21:41:07 +0200

    bugfix for the bugfix

--- src/designmatrix.jl
+++ src/designmatrix.jl
@@ -155,7 +155,7 @@ end
 
 function changeMatSize!(m, fe, remats)
     changeReMatSize!.(remats, Ref(m))
-    fe = SparseMatrixCSC(m, fe.dims.n, fe.colptr, fe.rowval, fe.nzval)
+    fe = SparseMatrixCSC(m, size(fe,1), fe.colptr, fe.rowval, fe.nzval)
     return (fe, remats...)
 end
 function changeReMatSize!(remat::MixedModels.AbstractReMat, m::Integer)
