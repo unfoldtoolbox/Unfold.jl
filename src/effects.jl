f6ec849a8f6081e6b487bd5ba0ad1455079dc84e
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-11-05 23:14:59 +0100

    initial working (for MassUnivariate) effect interface. nice

--- src/effects.jl
+++ src/effects.jl
@@ -1,17 +1,42 @@
 import Effects: effects
 using Effects
+
+# Implementation based on Effects Package; likely should repackage in UnfoldEffects; but then...
 function effects(design::AbstractDict, model::UnfoldModel;
-    eff_col=nothing, err_col=:err, typical=mean,
-    lower_col=:lower, upper_col=:upper)
-grid = Effects._reference_grid(design)
-dv = something(eff_col, :yhat)
-effects!(grid, model; eff_col=dv, err_col=err_col, typical=typical)
-# XXX DataFrames dependency
-grid[!, lower_col] = grid[!, dv] - grid[!, err_col]
-grid[!, upper_col] = grid[!, dv] + grid[!, err_col]
-return grid
-# up_low = let dv = getproperty(reference_grid, dv), err = getproperty(reference_grid, err_col)
-#     (; lower_col => dv .- err, upper_col => dv .+ err)
-# end
-# return (; reference_grid..., up_low...)
+    typical=mean)
+    reference_grid = Effects._reference_grid(design)
+    form = formula(model) # get formula
+    form_typical = Effects.typify(reference_grid, form, modelmatrix(model); typical=typical) # replace non-specified fields with "constants"
+    X = modelcols(form_typical, reference_grid) # get model cols
+    eff = yhat(model,X) # apply coefficients
+    
+    # because coefficients are 2D/3D arry, we have to cast it correctly to one big dataframe
+    result = DataFrame(cast_referenceGrid(reference_grid,eff,times(model)[1]) )
+    
+return result   
+end
+
+function cast_referenceGrid(r,eff,times)
+    nchan = size(eff, 2)
+    neff = size(r,1)
+    neffCol = size(r,2)
+    ncols = size(eff,1) ÷ neff # typically ntimes
+   
+    # replicate
+    # for each predictor in r
+    coefs_rep = Array{Float64}(undef,nchan,ncols,neff,neffCol)
+    for k = 1:neffCol
+        # repeat it for nchan + ncols
+        coefs_rep[:,:,:,k] = permutedims(repeat(r[:,k], outer = [1, nchan, ncols]), [2, 3, 1])
+    end
+    colnames_basis_rep = permutedims(repeat(times, 1, nchan, neff), [2 1 3])
+    chan_rep = repeat(1:nchan, 1, ncols, neff)
+    result = Dict(   :yhat => linearize(eff),
+            :time => linearize(colnames_basis_rep),
+            :channel => linearize(chan_rep))
+   
+    for k = 1:neffCol
+            push!(result,Symbol(names(r)[k]) =>linearize(coefs_rep[:,:,:,k]))
+    end
+    return result
 end
