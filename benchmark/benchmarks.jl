bc6e8edb0657f064b34d2d2b5570116140d6c57f
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2021-11-23 21:15:41 +0100

    adding git status?

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,12 +1,6 @@
 using BenchmarkTools
-using Pkg
-tempdir = mktempdir()
-Pkg.activate(tempdir)
-Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
-Pkg.add(["BenchmarkTools", "PkgBenchmark","Random"])
-Pkg.resolve()
-
 using Random
+
 const SUITE = BenchmarkGroup()
 Random.seed!(3)
 
@@ -19,3 +13,4 @@ SUITE["dc"] = BenchmarkGroup(["dc"])
 # designmatrix generation
 SUITE["dc"]["X_gen_lin"] = @benchmarkable 1==1
 SUITE["nodc"]["fit_lin"] = @benchmarkable 1==2
+run(`git status`)
