3d0e073338151d075921ac36c6afb1fd736a77ba
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-02-09 15:42:47 +0100

    bit of cleanup

--- test/effects.jl
+++ test/effects.jl
@@ -184,17 +184,3 @@ end
 	@test all(eff.yhat[1:2, 5:end] .== 0)
 end
 
-
-
-begin
-	data, evts = loadtestdata("test_case_4a") #
-	evts[!,:continuousA] = rand(MersenneTwister(42),nrow(evts))
-	evts[!,:continuousB] = ["m","x"][Int.(1 .+ round.(rand(MersenneTwister(43),nrow(evts))))]
-	b1 = firbasis(τ = (0.0, 0.25), sfreq = 20, name = "basisA")
-	b2 = firbasis(τ = (1.0, 1.15), sfreq = 20, name = "basisB")
-	f1 = @formula 0 ~ 1+continuousA*continuousB # 1
-	f2 = @formula 0 ~ 1+continuousB # 1
-	m_tul = fit(UnfoldModel, Dict("eventA"=>(f1,b1),"eventB"=>(f2,b2)), evts, data,eventcolumn="type")
-	
-	
-end
