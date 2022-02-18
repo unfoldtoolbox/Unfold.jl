cc578f33a1dd57df43aa66626b42feee521f12b5
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-02-18 21:42:47 +0100

    cleaned up test

--- test/statistics.jl
+++ test/statistics.jl
@@ -1,16 +1,18 @@
 @testset "LMM LRT" begin
     data,evts = loadtestdata("testCase3",dataPath="data");
+    data = reshape(data,1,length(data)) # add second channel
+    data = vcat(data,data);
     
         evts[!,:subject] .= string.(evts.subject);
-        data_epoch,times = Unfold.epoch(data=data.+1.1.*randn(size(data)),tbl=evts,τ=(-0.1,0.3),sfreq=10);
+        data_epoch,times = Unfold.epoch(data=data.+1.1.*randn(MersenneTwister(1),size(data)),tbl=evts,τ=(-0.1,0.3),sfreq=10);
         evts_epoch,data_epoch = Unfold.dropMissingEpochs(evts,data_epoch)
    
         
         f0  = @formula 0~1+condA + (1|subject);
         f1  = @formula 0~1+condA+condB + (1|subject);
             
-        m1 = fit(UnfoldModel,Dict(Any=>(f0,times)),evts_epoch,data_epoch);
-        m2 = fit(UnfoldModel,Dict(Any=>(f1,times)),evts_epoch,data_epoch);
+        m0 = fit(UnfoldModel,Dict(Any=>(f0,times)),evts_epoch,data_epoch);
+        m1 = fit(UnfoldModel,Dict(Any=>(f1,times)),evts_epoch,data_epoch);
         
         evts[!,:y] = data_epoch[1,1,:]
 
@@ -20,13 +22,12 @@
         lmm0 = fit(MixedModel,f0,evts)
         lmm1 = fit(MixedModel,f1,evts)
 
-        uf_lrt = likelihoodratiotest(m1,m2)
-        uf_lrt[1] == MixedModels.likelihoodratiotest(lmm0,lmm1)
-
-
-        @test MixedModels.likelihoodratiotest(lmm0,lmm1).pvalues ≈ uf_lrt[1].pvalues
-
+        uf_lrt = likelihoodratiotest(m0,m1)
+        mm_lrt = MixedModels.likelihoodratiotest(lmm0,lmm1)
+        
+        @test mm_lrt.pvalues ≈ uf_lrt[1].pvalues
 
         
+
         
 end
