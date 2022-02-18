2d7dbe6c54237fb49ecd07dde28a81a66f113674
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-02-18 22:33:17 +0100

    improved docstring

--- src/statistics.jl
+++ src/statistics.jl
@@ -45,12 +45,16 @@ end
 
     # Examples
     julia> pvalues(likelihoodratiotest(m1,m2))
+
     where m1/m2 are UnfoldLinearMixedModel's
     
     Tipp: if you only compare two models you can easily get a vector of p-values:
+
     julia> vcat(pvalues(likelihoodratiotest(m1,m2))...)
     
+    
     Multiple channels are returned linearized at the moment, as we do not have access to the amount of channels after the LRT, you can do:
+    
     julia> reshape(vcat(pvalues(likelihoodratiotest(m1,m2))...),ntimes,nchan)'
     
     """
