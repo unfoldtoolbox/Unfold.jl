ff5e98ac90f3e0d358bcce74f883e74fe8bd0292
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-03-21 23:15:35 +0100

    removed the initial coefficient placement

--- src/solver.jl
+++ src/solver.jl
@@ -171,21 +171,22 @@ function solver_robust(
             dd = view(data, ch, t,:)
             ix = @. !ismissing(dd)
 			# init beta
-			if ch == 1 && t==1
-			elseif ch > 1 && t == 1
-				copyto!(view(beta, ch, 1,:), view(beta, ch-1, 1,:))
-			else
-			 	copyto!(view(beta, ch,t, :), view(beta, ch,t-1, :))
-			end
+			#if ch == 1 && t==1
+			#elseif ch > 1 && t == 1
+			#	copyto!(view(beta, ch, 1,:), view(beta, ch-1, 1,:))
+			#else
+			# 	copyto!(view(beta, ch,t, :), view(beta, ch,t-1, :))
+			#end
 			
 			X_local = disallowmissing((X[ix,:])) # view crashes robust model here. XXX follow up once 
             # https://github.com/JuliaStats/GLM.jl/issues/470 received a satisfying result
 			y_local = disallowmissing(@view(data[ch,t,ix]))
 
             # if not specified otherwise
-			if :initial_coef ∉ keys(rlmOptions)
-                rlmOptions = merge(rlmOptions,(initial_coef=@view(beta[ch,t,:]),))
-            end
+            # this results in strange regularizing effects
+			#if :initial_coef ∉ keys(rlmOptions)
+            #    rlmOptions = merge(rlmOptions,(initial_coef=@view(beta[ch,t,:]),))
+            #end
 			m = rlm(X_local,y_local,estimator;rlmOptions...)
 			beta[ch,t,:] .= coef(m)
             
