9f71c2500e1faebc8825b1e37a5af63b2d1fc577
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2022-03-22 21:32:36 +0100

    Forgot to remove debug statements...

--- src/utilities.jl
+++ src/utilities.jl
@@ -57,8 +57,7 @@ function epoch(
         #d_start = eventonset
         d_start = Int(round(tbl[si, eventtime] + times[1].*sfreq))
         d_end = Int(round(tbl[si, eventtime] +times[end].*sfreq))
-        @info d_start
-        @info d_end
+        
         e_start = 1
         e_end = lenEpochs
         #println("d: $(size(data)),e: $(size(epochs)) | $d_start,$d_end,$e_start,$e_end | $(tbl[si,eventtime])")
