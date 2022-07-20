8e5651bf9906f1763a609b4f0985c40cd73948b7
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-07-20 11:53:52 +0200

    simplified spline script

--- src/splinepredictors.jl
+++ src/splinepredictors.jl
@@ -1,18 +1,11 @@
 using StatsModels
 import Statistics.quantile
-#import Splines2.bs
+
 import Base.show
 using Random
 using BSplines
 const bsPLINE_CONTEXT = Any
-#x = Random.rand(20)
-#x = collect(range(0,stop=10,step=0.1))
-#y = log.(x)
-#scatter(x,y)
 
-#using Splines2
-#f = Splines2.bs_(x,df=5,intercept=true)
-#f(x)
 
 
 function offsetArrayToZeros!(oneRow, spl)
@@ -48,44 +41,44 @@ end
 
 
 #spl(x,df) = Splines2.bs(x,df=df,intercept=true) # assumes intercept
-spl(x, df) = 1
+spl(x, df) = 1 # fallback
 
 # make a nice call if the function is called via REPL
-spl(t::Symbol, d::Int) = uf_bsplineTerm(term(t), term(d))
+spl(t::Symbol, d::Int) = BSplineTerm(term(t), term(d))
 
-mutable struct uf_bsplineTerm{T,D} <: AbstractTerm
+mutable struct BSplineTerm{T,D} <: AbstractTerm
     term::T
     df::D
     fun::Any # function handle
 end
-function uf_bsplineTerm(term, df)
-    uf_bsplineTerm(term, df, nothing)
+function BSplineTerm(term, df)
+    BSplineTerm(term, df, nothing)
 end
 
 
-Base.show(io::IO, p::uf_bsplineTerm) = print(io, "spl($(p.term), $(p.df))")
+Base.show(io::IO, p::BSplineTerm) = print(io, "spl($(p.term), $(p.df))")
 
 function StatsModels.apply_schema(
     t::FunctionTerm{typeof(spl)},
     sch::StatsModels.Schema,
     Mod::Type{<:bsPLINE_CONTEXT},
 )
-    apply_schema(uf_bsplineTerm(t.args_parsed...), sch, Mod)
+    apply_schema(BSplineTerm(t.args_parsed...), sch, Mod)
 end
 function StatsModels.apply_schema(
-    t::uf_bsplineTerm,
+    t::BSplineTerm,
     sch::StatsModels.Schema,
     Mod::Type{<:bsPLINE_CONTEXT},
 )
     term = apply_schema(t.term, sch, Mod)
     isa(term, ContinuousTerm) ||
-        throw(ArgumentError("uf_bsplineTerm only works with continuous terms (got $term)"))
+        throw(ArgumentError("BSplineTerm only works with continuous terms (got $term)"))
 
     isa(t.df, ConstantTerm) ||
-        throw(ArgumentError("uf_bsplineTerm df must be a number (got $(t.df))"))
-    uf_bsplineTerm(term, t.df.n)
+        throw(ArgumentError("BSplineTerm df must be a number (got $(t.df))"))
+    BSplineTerm(term, t.df.n)
 end
-function StatsModels.modelcols(p::uf_bsplineTerm, d::NamedTuple)
+function StatsModels.modelcols(p::BSplineTerm, d::NamedTuple)
 
     col = modelcols(p.term, d)
 
@@ -97,10 +90,11 @@ function StatsModels.modelcols(p::uf_bsplineTerm, d::NamedTuple)
     X = p.fun(col)
 
     # remove middle X to negate intercept = true, generating a pseudo effect code 
-    X[:, Not(Int(ceil(end / 2)))]
+    return X[:, Not(Int(ceil(end / 2)))]
 end
-#StatsModels.terms(p::uf_bsplineTerm) = terms(p.term)
-StatsModels.termvars(p::uf_bsplineTerm) = StatsModels.termvars(p.term)
-StatsModels.width(p::uf_bsplineTerm) = p.df
-StatsModels.coefnames(p::uf_bsplineTerm) =
+
+#StatsModels.terms(p::BSplineTerm) = terms(p.term)
+StatsModels.termvars(p::BSplineTerm) = StatsModels.termvars(p.term)
+StatsModels.width(p::BSplineTerm) = p.df
+StatsModels.coefnames(p::BSplineTerm) =
     "spl(" .* coefnames(p.term) .* "," .* string.(1:p.df) .* ")"
