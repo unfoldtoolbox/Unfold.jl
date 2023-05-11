f204d6baeead5950c572eab79a97c576d72f58cf
jschepers <judith.schepers@vis.uni-stuttgart.de>
2023-05-11 15:59:58 +0000

    uncommented tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,25 @@
 include("setup.jl")
 
 
-#@testset "BasisFunctions" begin
-#    include("basisfunctions.jl")
-#end
+@testset "BasisFunctions" begin
+    include("basisfunctions.jl")
+end
 
-#@testset "Fitting" begin
-#    include("fit.jl")
-#end
+@testset "Fitting" begin
+    include("fit.jl")
+end
 
-#@testset "Designmatrix" begin
-#    include("designmatrix.jl")
-#end
+@testset "Designmatrix" begin
+    include("designmatrix.jl")
+end
 
-#@testset "Splines" begin
-#    include("splines.jl")
-#end
+@testset "Splines" begin
+    include("splines.jl")
+end
 
-#@testset "Predict" begin 
-#    include("predict.jl")
-#end
+@testset "Predict" begin 
+    include("predict.jl")
+end
 
 @testset "Effects" begin
     include("effects.jl")
