ea60e0a64810146ed75bba322038ddc8d3a05a99
Documenter.jl <documenter@juliadocs.github.io>
2023-10-25 09:32:43 +0000

    build based on 0c92e6e

--- dev/_literate/HowTo/pyjulia_unfold.jl
+++
@@ -1,3 +0,0 @@
-# ## Using Unfold.jl from Python
-# it is straight forward to call Unfold from Python using `JuliaCall`.
-# Please find a notebook [here](https://github.com/unfoldtoolbox/Unfold.jl/blob/main/docs/src/HowTo/pyjulia_unfold.ipynb)
