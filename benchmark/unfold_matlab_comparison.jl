d88ab3e58d968b595238e89d3bf6a94f730f6b25
ReneSkukies <ReneSkukies@gmx.de>
2023-11-22 15:15:08 +0000

    Restructure the code a bit

--- benchmark/unfold_matlab_comparison.jl
+++ benchmark/unfold_matlab_comparison.jl
@@ -7,42 +7,10 @@ using BSplineKit, Unfold, UnfoldSim
 using Random
 
 mat"addpath('/store/users/skukies/unfold_duration/lib/eeglab')" # Add EEGLAB?
-mat"addpath('/store/users/skukies/unfold_duration/lib/unfold')" # Add Unfold?
+mat"addpath('/store/users/skukies/unfold_duration/lib/unfold')" #" Add Unfold?
 mat"init_unfold"
 
-## Julia
-# simulate data
-# data, events = UnfoldSim.predef_eeg()
-design = SingleSubjectDesign(;
-        conditions=Dict(:condition=>["car","face"],:continuous=>range(-5,5,length=10))
-        ) |> x->RepeatDesign(x,50);
-
-p1 =  LinearModelComponent(;
-        basis = p100(),
-        formula = @formula(0~1),
-        β = [5]
-);
-
-n1 =  LinearModelComponent(;
-        basis = n170(),
-        formula = @formula(0~1+condition),
-        β = [5,-3]
-);
-
-p3 =  LinearModelComponent(;
-        basis = p300(),
-        formula = @formula(0~1+continuous),
-        β = [5,1]
-);
-
-components = [p1,n1,p3]
-data,events = simulate(MersenneTwister(1),design,components,UniformOnset(;width=500,offset=200),PinkNoise());
-
-# Run fit two times; once for compilation and once for actual timing
-m = fit(UnfoldModel,Dict(Any=>(@formula(0~1+condition+continuous),firbasis(τ=[-0.1,1],sfreq=100,name="basis"))),events,data);
-@time m = fit(UnfoldModel,Dict(Any=>(@formula(0~1+condition+spl(continuous,5)),firbasis(τ=[-0.1,1],sfreq=100,name="basis"))),events,data);
-
-## Matlab part
+# Matlab function for comparison
 function calc_matlab(datajl, eventsjl)
     mat"
         EEG = eeg_emptyset();
@@ -77,13 +45,47 @@ function calc_matlab(datajl, eventsjl)
         EEG = uf_glmfit(EEG);
         toc
     "
-    end
+end
+
+
+#" Setting up data
+# data, events = UnfoldSim.predef_eeg()
+design = SingleSubjectDesign(;
+        conditions=Dict(:condition=>["car","face"],:continuous=>range(-5,5,length=10))
+        ) |> x->RepeatDesign(x,50);
+
+p1 =  LinearModelComponent(;
+        basis = p100(),
+        formula = @formula(0~1),
+        β = [5]
+);
+
+n1 =  LinearModelComponent(;
+        basis = n170(),
+        formula = @formula(0~1+condition),
+        β = [5,-3]
+        );
+        
+        p3 =  LinearModelComponent(;
+        basis = p300(),
+        formula = @formula(0~1+continuous),
+        β = [5,1]
+        );
+        
+components = [p1,n1,p3]
+data,events = simulate(MersenneTwister(1),design,components,UniformOnset(;width=500,offset=200),PinkNoise());
+
+# Run fit two times; once for compilation and once for actual timing
+m = fit(UnfoldModel,Dict(Any=>(@formula(0~1+condition+continuous),firbasis(τ=[-0.1,1],sfreq=100,name="basis"))),events,data);
+@time m = fit(UnfoldModel,Dict(Any=>(@formula(0~1+condition+spl(continuous,5)),firbasis(τ=[-0.1,1],sfreq=100,name="basis"))),events,data);
+
+## Matlab part
 
 calc_matlab(data, events)
 
 # Above tested 2023/11/22; Julia: 0.12 sec ; Matlab: 0.15sec
 
-# Multichannel w/ headmodel
+## Multichannel w/ headmodel
 
 c = LinearModelComponent(;basis=p100(),formula = @formula(0~1+condition),β = [5,1]);
 c2 = LinearModelComponent(;basis=p300(),formula = @formula(0~1+continuous),β = [5,-3]);
@@ -101,6 +103,5 @@ data_mc,events_mc = simulate(MersenneTwister(1),design, [mc,mc2],  onset, PinkNo
 
 # Fit Matlab
 calc_matlab(data_mc, events_mc)
-
 
 # Above tested 2023/11/22; Julia: ca.8.9 sec ; Matlab: ca. 10.5sec
