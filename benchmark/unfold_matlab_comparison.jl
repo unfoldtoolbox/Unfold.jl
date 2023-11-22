6f4623ed8f44dfb30a10326d736192ebb116a408
ReneSkukies <ReneSkukies@gmx.de>
2023-11-22 15:10:50 +0000

    Adding multi channel comparison

--- benchmark/unfold_matlab_comparison.jl
+++ benchmark/unfold_matlab_comparison.jl
@@ -15,7 +15,7 @@ mat"init_unfold"
 # data, events = UnfoldSim.predef_eeg()
 design = SingleSubjectDesign(;
         conditions=Dict(:condition=>["car","face"],:continuous=>range(-5,5,length=10))
-        ) |> x->RepeatDesign(x,100);
+        ) |> x->RepeatDesign(x,50);
 
 p1 =  LinearModelComponent(;
         basis = p100(),
@@ -36,47 +36,71 @@ p3 =  LinearModelComponent(;
 );
 
 components = [p1,n1,p3]
-data,events = simulate(MersenneTwister(1),design,[p1,n1,p3],UniformOnset(;width=500,offset=200),PinkNoise());
+data,events = simulate(MersenneTwister(1),design,components,UniformOnset(;width=500,offset=200),PinkNoise());
 
 # Run fit two times; once for compilation and once for actual timing
 m = fit(UnfoldModel,Dict(Any=>(@formula(0~1+condition+continuous),firbasis(τ=[-0.1,1],sfreq=100,name="basis"))),events,data);
 @time m = fit(UnfoldModel,Dict(Any=>(@formula(0~1+condition+spl(continuous,5)),firbasis(τ=[-0.1,1],sfreq=100,name="basis"))),events,data);
 
 ## Matlab part
+function calc_matlab(datajl, eventsjl)
+    mat"
+        EEG = eeg_emptyset();
+        
+        EEG.data = $datajl;
+        EEG.srate = 100;
+    "
 
-mat"
-    EEG = eeg_emptyset();
-    EEG.data = repmat($data',30,1);
-    EEG.data = EEG.data + randn(size(EEG.data));
-    EEG.srate = 100;
-"
+    mat_events = mxarray(Dict("continuous"=>eventsjl.continuous, "condition"=>eventsjl.condition, "latency"=>eventsjl.latency))
 
-mat_events = mxarray(Dict("continuous"=>events.continuous, "condition"=>events.condition, "latency"=>events.latency))
-is_struct(mat_events)
-mat"class($mat_events)"
-mat"disp($mat_events)"
+    mat"
+        for e = 1:length($mat_events.latency)
+            EEG.event(end+1).latency =  $mat_events.latency(e);
+            EEG.event(end).condition =  $mat_events.condition{e};
+            EEG.event(end).continuous=  $mat_events.continuous(e);
+            EEG.event(end).continuousB=  rand(1);
+            EEG.event(end).type = 'fixation';
+        end
+    "
 
-mat"
-    for e = 1:length($mat_events.latency)
-        EEG.event(end+1).latency =  $mat_events.latency(e);
-        EEG.event(end).condition =  $mat_events.condition{e};
-        EEG.event(end).continuous=  $mat_events.continuous(e);
-        EEG.event(end).continuousB=  rand(1);
-        EEG.event(end).type = 'fixation';
+    mat"EEG = eeg_checkset(EEG)"
+
+    mat"    
+        cfgDesign = [];
+        cfgDesign.eventtypes = {'fixation'};
+        cfgDesign.formula = 'y ~ 1+ cat(condition)+spl(continuous,5)';
+        tic
+        EEG = uf_designmat(EEG,cfgDesign);
+
+        EEG = uf_timeexpandDesignmat(EEG,'timelimits',[-1,1]);
+        toc
+        EEG = uf_glmfit(EEG);
+        toc
+    "
     end
-"
 
-mat"EEG = eeg_checkset(EEG)"
+calc_matlab(data, events)
 
-mat"    
-    cfgDesign = [];
-    cfgDesign.eventtypes = {'fixation'};
-    cfgDesign.formula = 'y ~ 1+ cat(condition)+spl(continuous,5)';
-    tic
-    EEG = uf_designmat(EEG,cfgDesign);
+# Above tested 2023/11/22; Julia: 0.12 sec ; Matlab: 0.15sec
 
-    EEG = uf_timeexpandDesignmat(EEG,'timelimits',[-1,1]);
-    toc
-    EEG = uf_glmfit(EEG);
-    toc
-"
+# Multichannel w/ headmodel
+
+c = LinearModelComponent(;basis=p100(),formula = @formula(0~1+condition),β = [5,1]);
+c2 = LinearModelComponent(;basis=p300(),formula = @formula(0~1+continuous),β = [5,-3]);
+
+hart = headmodel(type="hartmut")
+mc = UnfoldSim.MultichannelComponent(c, hart=>"Left Postcentral Gyrus")
+mc2 = UnfoldSim.MultichannelComponent(c2, hart=>"Right Occipital Pole")
+
+onset = UniformOnset(;width=500,offset=200);
+
+data_mc,events_mc = simulate(MersenneTwister(1),design, [mc,mc2],  onset, PinkNoise())
+
+# Fit Unfold.jl
+@time m = fit(UnfoldModel,Dict(Any=>(@formula(0~1+condition+spl(continuous,5)),firbasis(τ=[-0.1,1],sfreq=100,name="basis"))),events_mc,data_mc);
+
+# Fit Matlab
+calc_matlab(data_mc, events_mc)
+
+
+# Above tested 2023/11/22; Julia: ca.8.9 sec ; Matlab: ca. 10.5sec
