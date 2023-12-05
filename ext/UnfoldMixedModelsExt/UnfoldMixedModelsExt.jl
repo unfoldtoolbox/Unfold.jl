7a8a6c6f5ea2efdfe3792d711b842b3512941738
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2023-12-05 14:15:08 +0100

    Apply suggestions from formatter
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/UnfoldMixedModelsExt/UnfoldMixedModelsExt.jl
+++ ext/UnfoldMixedModelsExt/UnfoldMixedModelsExt.jl
@@ -6,9 +6,9 @@ import Unfold: isMixedModelFormula,make_estimate
 using MixedModels
 #import MixedModels.FeMat # extended for sparse femats, type piracy => issue on MixedModels.jl github
 using StaticArrays # for MixedModels extraction of parametrs (inherited from MixedModels.jl, not strictly needed )
-import MixedModels: likelihoodratiotest,ranef
+import MixedModels: likelihoodratiotest, ranef
 using StatsModels
-import StatsModels: fit!,coef,coefnames,modelcols,modelmatrix
+import StatsModels: fit!, coef, coefnames, modelcols, modelmatrix
 using SparseArrays
 using DocStringExtensions
 using LinearAlgebra # LowerTriangular
