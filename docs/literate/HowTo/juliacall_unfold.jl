110ad73e3c92aaf42e4fc4e6b516ef45fb7c47c1
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2024-06-13 16:40:19 +0200

    Update docs/literate/HowTo/juliacall_unfold.jl

--- docs/literate/HowTo/juliacall_unfold.jl
+++ docs/literate/HowTo/juliacall_unfold.jl
@@ -45,6 +45,6 @@
 # `@formula` - we havent found a way to call macros yet, even though we think it should be possible. For now please use `f = jl.seval("@formula(0~1+my+cool+design)")`. Later versions might support something like f = `@formula("0~1+my+cool+design)"` directly
 #
 # specifying the design: Since Unfold 0.7 we officially switched to the `["eventtypeA"=>(formula,basisfunction),"eventtypeB"=>(otherformula,otherbasisfunction)]` syntax from a Dict-based syntax. Unfortunately, `=>` (a pair) is not supported in Python and one needs to do some rewriting:
-`jl.convert(jl.Pair,(formula,basisfunction))` which makes the code less readable. We are thinking of ways to remedy this - but right now there is now way around. For now, it is also possible to use the old syntax {"eventtypeA"=>(formula,basisfunction),"eventtypeB"=>(otherformula,otherbasisfunction)} which is clearly easier to read :)
+# `jl.convert(jl.Pair,(formula,basisfunction))` which makes the code less readable. We are thinking of ways to remedy this - but right now there is now way around. For now, it is also possible to use the old syntax {"eventtypeA"=>(formula,basisfunction),"eventtypeB"=>(otherformula,otherbasisfunction)} which is clearly easier to read :)
 #
 # `UnfoldSim.design` special case in UnfoldSim design, we need a `Dict` with a `Symbol` , one has to do something like `condition_dict_jl = {convert(jl.Symbol,"condA"):["car", "face"]}` to do so. We will try to allow strings here as well, removing this constraint.
