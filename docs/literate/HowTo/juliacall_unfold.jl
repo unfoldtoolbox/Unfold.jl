875b04f5ed3d4e0eab395be6444fcc5724ddb435
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2024-06-19 09:58:43 +0200

    Update juliacall_unfold.jl

--- docs/literate/HowTo/juliacall_unfold.jl
+++ docs/literate/HowTo/juliacall_unfold.jl
@@ -42,9 +42,21 @@
 # ## Important limitations
 # Python doesnt not offer the full expressions that are available in Julia. So there are some things you need to give special attention:
 #
-# `@formula` - we havent found a way to call macros yet, even though we think it should be possible. For now please use `f = jl.seval("@formula(0~1+my+cool+design)")`. Later versions might support something like f = `@formula("0~1+my+cool+design)"` directly
+# **`@formula`**: we havent found a way to call macros yet, even though we think it should be possible. For now please use `f = jl.seval("@formula(0~1+my+cool+design)")`. Later versions might support something like `f = @formula("0~1+my+cool+design)"` directly
 #
-# specifying the design: Since Unfold 0.7 we officially switched to the `["eventtypeA"=>(formula,basisfunction),"eventtypeB"=>(otherformula,otherbasisfunction)]` syntax from a Dict-based syntax. Unfortunately, `=>` (a pair) is not supported in Python and one needs to do some rewriting:
-# `jl.convert(jl.Pair,(formula,basisfunction))` which makes the code less readable. We are thinking of ways to remedy this - but right now there is now way around. For now, it is also possible to use the old syntax {"eventtypeA"=>(formula,basisfunction),"eventtypeB"=>(otherformula,otherbasisfunction)} which is clearly easier to read :)
+# **Specifying the design**: Since Unfold 0.7 we officially switched to the 
+# ```julia
+# ["eventtypeA"=>(formula,basisfunction),
+# "eventtypeB"=>(otherformula,otherbasisfunction)]
+# ```
+# Array-based syntax, from a Dict-based syntax. Unfortunately, `=>` (a pair) is not supported in Python and one needs to do some rewriting:
+# ```python
+# jl.convert(jl.Pair,(formula,basisfunction))
+# ```
+# which makes the code less readable. We are thinking of ways to remedy this - but right now there is now way around. For now, it is also possible to use the old syntax e.g. in python 
+# ```python
+# {"eventtypeA"=>(formula,basisfunction),"eventtypeB"=>(otherformula,otherbasisfunction)}
+# ```
+# which is clearly easier to read :)
 #
-# `UnfoldSim.design` special case in UnfoldSim design, we need a `Dict` with a `Symbol` , one has to do something like `condition_dict_jl = {convert(jl.Symbol,"condA"):["car", "face"]}` to do so. We will try to allow strings here as well, removing this constraint.
+# **`UnfoldSim.design`**: we need a `Dict` with a `Symbol` , one has to do something like `condition_dict_jl = {convert(jl.Symbol,"condA"):["car", "face"]}` to do so. We will [try to allow strings}(https://github.com/unfoldtoolbox/UnfoldSim.jl/issues/96) here as well, removing this constraint.
