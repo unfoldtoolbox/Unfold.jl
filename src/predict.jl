fb06195c056b0f320983ed0364249f1c30f1158a
ReneSkukies <ReneSkukies@gmx.de>
2024-09-02 12:37:16 +0000

    up predict.jl
    update predict function for partial overlap, so that keep_basis input is converted to vector.

--- src/predict.jl
+++ src/predict.jl
@@ -260,7 +260,10 @@ end
     @assert !(!isempty(keep_basis) & !isempty(exclude_basis)) "can't have no overlap & specify keep/exclude at the same time. decide for either case"
     # Partial overlap! we reconstruct with some basisfunctions deactivated
     if !isempty(keep_basis)
+        if !isa(keep_basis, Vector); keep_basis = [keep_basis]; end # Check if keep_basis is a vector
+        @assert !isempty(intersect(basisname(Unfold.formulas(uf)), keep_basis)) "Couldn't find (any of) $keep_basis in the models basisnames"
         basisnames = keep_basis
+
     else
         basisnames = basisname(Unfold.formulas(uf))
         basisnames = setdiff(basisnames, exclude_basis)
