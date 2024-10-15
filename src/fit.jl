4d57950e9dc1d83e7170165a3970dea7381ae9e2
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2024-10-15 09:35:48 +0200

    better fit docs (#221)
    * better fit docs
    
    * docs: add maxvanmigem as a contributor for bug (#222)
    
    * docs: update README.md
    
    * docs: update .all-contributorsrc
    
    ---------
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * added contrast tutorial; better docs
    
    * forgot to undo a change sum/effects
    
    ---------
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>

--- src/fit.jl
+++ src/fit.jl
@@ -6,32 +6,52 @@
 
 Generates Designmatrix & fits model, either mass-univariate (one model per epoched-timepoint) or time-expanded (modeling linear overlap).
 
-- `eventcolumn` (Symbol/String, default :event) - the column in `tbl::AbstractDataFrame` to differentiate the basisfunctions as defined in `d::Vector{Pair}`
-- `show_progress` (Bool, default true) - show Progress via ProgressMeter
+## keyword arguments
+- `contrasts::Dict`: (default: `Dict()`) contrast to be applied to formula. Example: `Dict(:my_condition=>EffectsCoding())`. More information here: https://juliastats.org/StatsModels.jl/stable/contrasts/
+- `eventcolumn::Union{Symbol,String}` (default `:event`) - the column in `tbl` to differentiate the basisfunctions as defined in `d::Vector{Pair}`
+- `solver::function`: (default: `solver_defaut`). The solver used for `y=Xb`, e.g. `(X,y;kwargs...) -> solver_default(X,y;kwargs...)`
+- `show_progress::Bool` (default `true`) - show progress via ProgressMeter - passed to `solver`
+- `eventfields::Array: (optional, default `[:latency]`) Array of symbols, representing column names in `tbl`, which are passed to basisfunction event-wise. First field of array always defines eventonset in samples.
 
 If a `Vector[Pairs]` is provided, it has to have one of the following structures:
-`[:A=>(f,basisfunction), :B=>(f2,bf2)]` - for deconvolutin analyses (use `Any=>(f,bf)` to match all rows of `tbl` in one basis functins)
-`[:A=>(f,timesvector), :B=>(f2,timesvector)]` - for mass univariate analyses. If multiple rERPs are calculated at the same time, the timesvectors must be the same
-
+For **deconvolution** analyses (use `Any=>(f,bf)` to match all rows of `tbl` in one basis functions). Assumes `data` is a continuous EEG stream, either a `Vector` or a `ch x time` `Matrix`
+```julia
+f1 = @formula(0~1+my_condition)
+[
+ :A=>(f1,firbasis((-0.1,1),128), # sfreq = 128Hz
+ :B=>(f2,firbasis((-3,2),128)
+]
+```
+for **mass-univariate** analyses without deconvolution. Assumes `data` to be cut into epochs already (see `Unfold.epoch`). Follows *eeglab* standard `ch x time x trials`:
+```julia
+timesvector = range(-0.1,3,step=1/100)
+[
+ :A=>(f1,timesvector),
+ :B=>(f2,timesvector)
+]
+```
 
 ## Notes
-- The `type` can be specified directly as well e.g. `fit(type::UnfoldLinearModel)` instead of inferred
-- The data is reshaped if it is missing one dimension to have the first dimesion then `1` "Channel".
+- The `type` can be specified directly as well e.g. `fit(type::UnfoldLinearModel)` instead of relying on the automatic inference
+- The data is reshaped if it is missing one dimension to have the first dimension then `1` "Channel".
 
 ## Examples
 Mass Univariate Linear
 ```julia-repl
-julia> data,evts = loadtestdata("testCase1")
-julia> data_r = reshape(data,(1,:))
-julia> data_e,times = Unfold.epoch(data=data_r,tbl=evts,τ=(-1.,1.9),sfreq=10) # cut the data into epochs. data_e is now ch x times x epoch
+julia> data,evts = UnfoldSim.predef_eeg()
+julia> data_e,times = Unfold.epoch(data=data,tbl=evts,τ=(-1.,1.9),sfreq=100) # cut the data into epochs. data_e is now ch x times x epoch
 
-julia> f  = @formula 0~1+continuousA+continuousB # 1
+julia> f  = @formula 0~1+continuousA+continuousB 
 julia> model = fit(UnfoldModel,f,evts,data_e,times)
+# or:
+julia> model = fit(UnfoldModel,[Any=>(f,times)],evts,data_e)
 ```
 Timexpanded Univariate Linear
 ```julia-repl
-julia> basisfunction = firbasis(τ=(-1,1),sfreq=10,name="A")
-julia> model = fit(UnfoldModel,[Any=>(f,basisfunction],evts,data_r)
+julia> basisfunction = firbasis(τ=(-1,1),sfreq=10)
+julia> model = fit(UnfoldModel,f,evts,data,basisfunction)
+# or
+julia> model = fit(UnfoldModel,[Any=>(f,basisfunction],evts,data)
 ```
 
 """
