eac4a0af8c75db01db9ee1297be07314b573b3f5
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-11-03 22:14:42 +0100

    major bug in likelihoodratiotest, changed API,not breaking

--- ext/UnfoldMixedModelsExt/statistics.jl
+++ ext/UnfoldMixedModelsExt/statistics.jl
@@ -4,38 +4,45 @@ $(SIGNATURES)
 Returns a partial LMM model (non-functional due to lacking data) to be used in likelihoodratiotests.
 `k` to selcet which of the modelfit's to fake
 """
-function fake_lmm(m::UnfoldLinearMixedModel, k::Int)
+function fake_lmm(data::AbstractArray{<:Number,3}, m::UnfoldLinearMixedModel, k::Int)
     mm = modelmatrix(m)
     @assert length(mm) == 1 "LRT is currently not implemented for fitting multiple events at the same time"
+    fcoll = Unfold.modelfit(m)
     feterm = mm[1][1]
     #reterm = mm[2:end]
-    fakeY = zeros(size(feterm, 1))
-    lmm = LinearMixedModel_wrapper(Unfold.formulas(m), fakeY, mm[1])
+    #fakeY = zeros(size(feterm, 1))
+    channel = fcoll.fits[k].channel
+    time_ix = fcoll.fits[k].timeIX
+    y = data[channel, time_ix, :]
+    lmm = LinearMixedModel_wrapper(Unfold.formulas(m), y, mm[1])
 
-    fcoll = Unfold.modelfit(m)
-    #lmm.optsum.feval .= 1
-    #lmm.optsum.fmin .= 1
+    lmm.optsum.feval = 1
+    lmm.optsum.fmin = 1
     lmm.optsum.sigma = fcoll.fits[k].σ
     lmm.optsum.optimizer = :unfold
     lmm.optsum.returnvalue = :success
     setθ!(lmm, fcoll.fits[k].θ)
+    updateL!(lmm)
+    #MixedModels.setβ!(lmm, fcoll.fits[k].β)
     return lmm
 end
 
-fake_lmm(m::UnfoldLinearMixedModel) = fake_lmm.(m, 1:length(modelfit(m).fits))
+fake_lmm(data::AbstractArray, m::UnfoldLinearMixedModel) =
+    fake_lmm.(data, m, 1:length(modelfit(m).fits))
 
 """
 $(SIGNATURES)
 
 Calculate likelihoodratiotest
 """
-function MixedModels.likelihoodratiotest(m::UnfoldLinearMixedModel...)
+function MixedModels.likelihoodratiotest(data::AbstractArray, m::UnfoldLinearMixedModel...)
     #@info lrtest(fake_lmm.(m,1)...)
     n = length(Unfold.modelfit(m[1]).fits)
     lrt = Array{MixedModels.LikelihoodRatioTest}(undef, n)
     sizehint!(lrt, n)
     for k = 1:n
-        ms = fake_lmm.(m, k)
+
+        ms = fake_lmm.(Ref(data), m, k)
         #@info objective.(ms)
         lrt[k] = MixedModels.likelihoodratiotest(ms...)
     end
--- test/statistics.jl
+++ test/statistics.jl
@@ -11,7 +11,12 @@
     m0 = fit(UnfoldModel, [Any => (f0, times)], evts, data)
     m1 = fit(UnfoldModel, [Any => (f1, times)], evts, data)
 
-    tix = 1
+    uf_lrt = likelihoodratiotest(data, m0, m1)
+    uf_pval = reshape(vcat(pvalues(uf_lrt)...), size(data, 2), 2)'
+    @test uf_pval[1, :] == uf_pval[2, :]
+
+    #---
+    for tix = 1:3
     evts[!, :y] = data[1, tix, :]
 
     f0 = @formula y ~ 1 + condition + (1 | subject)
@@ -20,12 +25,13 @@
     lmm0 = fit(MixedModel, f0, evts)
     lmm1 = fit(MixedModel, f1, evts)
 
-    uf_lrt = likelihoodratiotest(m0, m1)
-    mm_lrt = MixedModels.likelihoodratiotest(lmm0, lmm1)
-
-    @test isapprox(mm_lrt.pvalues, uf_lrt[tix].pvalues, atol = 1e-20)
 
 
 
+        mm_lrt = MixedModels.likelihoodratiotest(lmm0, lmm1)
+        #---
+        @test isapprox(mm_lrt.pvalues[1], uf_pval[1, tix]; atol = 1e-10)
+        #---
+    end
 
 end
