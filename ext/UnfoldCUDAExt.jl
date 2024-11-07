a534b728e9450d53ad6f62c2e2e386f5d4357112
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-11-07 12:44:43 +0100

    removed automatic collect, only collect if necessary

--- ext/UnfoldCUDAExt.jl
+++ ext/UnfoldCUDAExt.jl
@@ -1,14 +1,18 @@
 module UnfoldCUDAExt
 using CUDA
+using Unfold
 using CUDA.CUSPARSE
+using LinearAlgebra
 
-function prepare(X, data::CuArray{T,2}) where {T<:Number}
+function Unfold.prepare(X, data::CuArray{T,2}) where {T<:Number}
     X_out = CuSparseMatrixCSC{T}(X)
     Y = CuArray{T}(data') # ch last!
     Ĥ = CUDA.zeros(T, size(Y, 2), size(X, 2))
     return Ĥ, Y, (X_out,)
 end
-function prepare(X, data::CuArray{T,3}) where {T<:Number}
+prepare_XTX(Ĥ, data::Adjoint{T,CuArray}, X) where {T} = prepare_XTX(Ĥ, CuArray(data), X)
+
+function Unfold.prepare(X, data::CuArray{T,3}) where {T<:Number}
     X_out = CuSparseMatrixCSC{T}(X)
     Y = CuArray{T}(permutedims(data, [3, 1, 2])) # trials - ch - time
     Ĥ = CUDA.zeros(T, size(Y, 2), size(Y, 3), size(X, 2))
