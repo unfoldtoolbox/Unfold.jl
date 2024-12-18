347975a0c5494608fa517e84eb525f1c21032bd6
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-12-18 20:15:31 +0100

    fix Interpolation=>interpolations

--- docs/literate/HowTo/FIRduration.jl
+++ docs/literate/HowTo/FIRduration.jl
@@ -12,7 +12,7 @@ evts.duration = 5:24
 
 
 # putting `scale_duration = Interpolation.Linear()` will introduce a Cameron-Hassall 2022 PNAS- Style basisfunction, that scales with the `:duration` column
-basisfunction = firbasis(τ = (-1, 2), sfreq = 5, scale_duration = Interpolation.Linear())
+basisfunction = firbasis(τ = (-1, 2), sfreq = 5, scale_duration = Interpolations.Linear())
 
 # Two examples with `duration = 10` 
 Unfold.kernel(basisfunction, [0, 10])
@@ -59,3 +59,4 @@ m = fit(UnfoldModel, bf_vec, evts, data; eventfields = [:latency, :duration]);
 heatmap(Matrix(modelmatrix(m))')
 
 p = predict(m; overlap = false)[1]
+heatmap(p[1, :, :])
