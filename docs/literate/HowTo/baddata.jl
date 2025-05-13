c578c4adc7ffac821528bad5d97723e9e3f1fc6b
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2025-05-13 09:45:30 +0200

    feature r2 (#272)
    * new tutorial, addressing #270
    
    * better plots
    
    * added r2
    
    * fix test, needed to unpack once
    
    * fix for ch x X data

---
+++ docs/literate/HowTo/baddata.jl
@@ -0,0 +1,48 @@
+# # Removing BAD data
+#
+# Sometimes one has bad data (artefacts etc.) in the data that one wants to remove prior to fitting
+# ### Setup
+# ```@raw html
+# <details>
+# <summary>Click to expand</summary>
+# ```
+## Load required packages
+using UnfoldSim
+using Unfold
+using CairoMakie
+using Random
+# ```@raw html
+# </details >
+# ```
+# ## Simulation
+# Let's start with a simulation of some data, and add some bad-data
+rng = MersenneTwister(1)
+data, evts = UnfoldSim.predef_eeg(n_repeats = 1)
+ix = 100:500
+data[ix] .+= 50 .* (rand(rng, length(ix)) .- 0.5)
+
+f = lines(data[1:1000])
+f
+
+# Clearly this data is bad and we should rather remove it!
+# We can use Julias `missing` data-type to indicate those portions.
+using Missings
+data_missing = allowmissing(data)
+data_missing[ix] .= missing
+
+lines(f.figure[2, 1], data_missing[1:1000])
+f
+# Nice, the data is sucessfully removed, without changing the overall size / timing of the data
+#
+# We can now fit a model with, and without the noise data
+m = fit(UnfoldModel, @formula(0 ~ 1), evts, data, firbasis((-0.3, 0.5), 100))
+m_missing =
+    fit(UnfoldModel, @formula(0 ~ 1), evts, data_missing, firbasis((-0.3, 0.5), 100))
+c = coeftable(m)
+c.group .= "with bad data"
+c_missing = coeftable(m_missing)
+c_missing.group .= "bad data removed"
+
+plot_erp(vcat(c, c_missing), mapping = (; color = :group))
+
+# Currently no helper function exists to translate e.g. MNE BAD segments/annotations automatically, but pull requests are very welcome!
